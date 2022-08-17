ce39e16fd3eb99a35b70af4958dea25a5bd57e29
SilverXuz <silver-xuz@mail.ru>
2022-08-17 21:53:21 +0300

    Программа, которая упорядочит по убыванию элементы каждой строки двумерного массива.

---
+++ Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8
+*/
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+int minNumber = matrix[0, 0];
+void Array(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0,100);
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(1) - 1; k++)
+            {
+                if(matr[i, k] > matr[i, k + 1])
+                {
+                    int x = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i, k] = x;                    
+                }
+            }
+        }
+    }
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Array(matrix);
+Console.WriteLine();
+FillArray(matrix);
