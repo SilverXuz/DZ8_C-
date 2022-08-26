536485ec039f775a8485a45ece168d4871fd2fc2
SilverXuz <silver-xuz@mail.ru>
2022-08-26 18:37:22 +0300

    Спираль

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -9,6 +9,7 @@
 10  9  8 7
 */ 
 
+//Решение из видео разбора от Дениса
 Console.Write("Введите кол-во строк: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите кол-во столбцов: ");
@@ -17,45 +18,95 @@ int[,] matrix = new int[n, m];
 
 void Array(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(1); i++)
+    int temp = 1, i = 0, j = 0;
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = matrix[i, j] + i + j;
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i++;
+    }
+    for (i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (j = 0; j < matrix.GetLength(1); j++)
             Console.Write(matrix[i, j] + " \t");
-        }
         Console.WriteLine();
     }
-
 }
 
-// void SpiralArray(int[,] matr)
+
+Array(matrix);
+
+// Решение от преподавателя
+// int arraySizeX = 7;
+// int arraySizeY = 4;
+// int[,] testArray = new int[arraySizeX, arraySizeY];
+
+// Console.Clear();
+// Console.WriteLine($"Задача 62.");
+// FillArrayInSpiral(testArray);
+// PrintDoubleArray(testArray);
+
+// void FillArrayInSpiral(int[,] array)
 // {
-//     for (int i = 0; i < matr.GetLength(0); i++)
+
+//     int posI = 0;
+//     int posJ = 0;
+//     bool isItNewCircle = true;
+
+//     for (int i = 0; i < array.Length; i++)
 //     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
+//         array[posI, posJ] = i + 1;
+
+//         if (posJ + 1 < array.GetLength(1) && isItNewCircle && array[posI, posJ + 1] == 0)
 //         {
-//             for (int k = 0; k < matr.GetLength(1) - 1; k++)
-//             {
-//                 if(matr[i, k] > matr[i, k + 1])
-//                 {
-//                     int x = matr[i, k + 1];
-//                     matr[i, k + 1] = matr[i, k];
-//                     matr[i, k] = x;                    
-//                 }
-//             }
+//             posJ++;
+//             continue;
+//         }
+//         else
+//         {
+//             isItNewCircle = false;
+//         }
+        
+//         if (posI + 1 < array.GetLength(0) && array[posI + 1, posJ] == 0)
+//         {
+//             posI++;
+//             continue;
+//         }
+        
+//         if (posJ - 1 >= 0 && array[posI, posJ - 1] == 0)
+//         {
+//             posJ--;
+//             continue;
+//         }
+        
+//         if (posI - 1 >= 0 && array[posI-1, posJ] == 0)
+//         {
+//             posI--;
+//             continue;
+//         }
+//         else
+//         {
+//             posJ++;
+//             isItNewCircle = true;
 //         }
 //     }
-//     for (int i = 0; i < matr.GetLength(0); i++)
+// }
+
+// void PrintDoubleArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
 //     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
+//         for (int j = 0; j < array.GetLength(1); j++)
 //         {
-//             Console.Write(matr[i, j] + " \t");
+//             Console.Write(array[i, j] + "\t");
 //         }
 //         Console.WriteLine();
 //     }
 // }
-
-Array(matrix);
-// Console.WriteLine();
-// SpiralArray(matrix);
