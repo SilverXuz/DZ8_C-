89eeba4fe5dcb2c153a758767de8a7a6b7a28078
SilverXuz <silver-xuz@mail.ru>
2022-08-26 19:00:08 +0300

    Треугольник паскаля

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -24,3 +24,30 @@ for (int y = 0; y < n; y++)
     Console.WriteLine();
 }
 Console.WriteLine();
+
+
+// Решение преподавателя
+
+// Console.Write("Введите число элементов в основании треугольника Паскаля ");
+// int n = int.Parse(Console.ReadLine() ?? "");
+// int i, c;
+// float factorial(int n)
+// {
+//     float i, x = 1;
+//     for (i = 1; i <= n; i++)
+//         x *= i;
+//     return x;
+//     }
+// for (i = 0; i < n; i++)
+// {
+//     for (c = 0; c <= (n - i); c++) 
+//         Console.Write(" ");
+//     for (c = 0; c <= i; c++)
+//     {
+//         Console.Write(" ");
+//         Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); 
+//     }
+//     Console.WriteLine();
+//     Console.WriteLine(); 
+// }
+// Console.ReadLine();
